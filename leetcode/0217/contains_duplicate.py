bb852b0cc35d403c4caf5ae6cf90e5a3d040c05c
huangkai <h1770360848@outlook.com>
2020-08-25 00:06:20 +0800

    feat: 增加0217问题排序解法

--- leetcode/0217/contains_duplicate.py
+++ leetcode/0217/contains_duplicate.py
@@ -18,6 +18,7 @@ class Solution:
     * 空间复杂度: O(N)，其中N为数组中的元素数量，主要为创建哈希表的开销
 
     """
+
     @classmethod
     def contains_duplicate(cls, nums: List[int]) -> bool:
         s = set()
@@ -26,4 +27,24 @@ class Solution:
                 return True
 
             s.add(num)
+        return False
+
+
+class Solution1:
+    """排序
+
+    在对数字从小到大排序之后，数组的重复元素一定出现在相邻位置中
+    遍历已排序的数组，判断相邻的两个元素是否相等，如果相等则说明存在重复的元素
+
+    * 时间复杂度: O(NlogN)，其中N为数组中的元素数量，需要对数组进行排序
+    * 空间复杂度: O(logN)，其中N为数组中的元素数量
+
+    """
+
+    @classmethod
+    def contains_duplicate(cls, nums: List[int]) -> bool:
+        nums.sort()
+        for index in range(len(nums) - 1):
+            if nums[index] == nums[index + 1]:
+                return True
         return False
